82892e1cf1b514c1dab3b9d89762d25751ba1b2b
bkelly16 <bkelly@45drives.com>
2021-03-15 21:16:02 -0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Initialize ansible varibles and inventory
+cp group_vars/metrics.yml.sample group_vars/metrics.yml
+cp group_vars/exporters.yml.sample group_vars/exporters.yml
+cp hosts.sample hosts
