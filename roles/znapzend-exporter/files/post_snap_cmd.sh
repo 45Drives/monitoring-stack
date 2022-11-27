6a001211c944b3baf23c921f919c38ab7fc8608a
bkelly16 <bkelly@45drives.com>
2022-11-27 13:17:14 -0400

    helper scripts remote-host option

--- roles/znapzend-exporter/files/post_snap_cmd.sh
+++ roles/znapzend-exporter/files/post_snap_cmd.sh
@@ -3,15 +3,11 @@
 EXPORTER_HOST="localhost"
 EXPORTER_PORT="9101"
 DATASET_NAME="$1"
-REMOTE_HOSTNAME="$2"
 
 if [ $# -eq 0 ]; then
     echo "Dataset name required. ex) storage/dataset1"
     exit 1
 fi
 
-if [ -z $REMOTE_HOSTNAME ];then
-    REMOTE_HOSTNAME="remote-host"
-fi
 
 /usr/bin/curl -sS $EXPORTER_HOST:$EXPORTER_PORT/postsnap/$DATASET_NAME
--- roles/znapzend-exporter/files/pre_send_cmd.sh
+++ roles/znapzend-exporter/files/pre_send_cmd.sh
@@ -3,10 +3,15 @@
 EXPORTER_HOST="localhost"
 EXPORTER_PORT="9101"
 DATASET_NAME="$1"
+REMOTE_HOSTNAME="$1"
 
 if [ $# -eq 0 ]; then
     echo "Dataset name required. ex) storage/dataset1"
     exit 1
+fi
+
+if [ -z $REMOTE_HOSTNAME ];then
+    REMOTE_HOSTNAME="remote-host"
 fi
 
 /usr/bin/curl -sS $EXPORTER_HOST:$EXPORTER_PORT/presend/$DATASET_NAME?TargetHost=$REMOTE_HOSTNAME
